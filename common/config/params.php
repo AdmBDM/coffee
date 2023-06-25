8869c0eb742908b46c20067f81cdfd34e2482b5b
Dmitriy Belik <Jim7rfp@gmail.com>
2023-06-25 15:56:44 +0300

    Coffee. + начальный контент, структура css. 2023-06-25

--- common/config/params.php
+++ common/config/params.php
@@ -12,11 +12,55 @@ return [
 	'brand_path' => '/images/brand/',
 	'prefix_blocks_id' => 'block',
 	'blocks' => [
-		'1' => 'Ремонт',
-		'2' => 'Обслуживание',
-		'3' => 'Гарантия',
-		'4' => 'Марки',
-		'5' => 'Для связи',
+		'0' => [
+			'title_full' => '',
+			'title_short' => '',
+			'on' => true,
+			'nav_menu' => false,
+			'label' => false
+		],
+		'1' => [
+			'title_full' => 'Профессиональный ремонт',
+			'title_short' => 'Ремонт',
+			'on' => true,
+			'nav_menu' => true,
+			'label' => true
+		],
+		'2' => [
+			'title_full' => 'Обслуживание',
+			'title_short' => 'Обслуживание',
+			'on' => true,
+			'nav_menu' => true,
+			'label' => true
+		],
+		'3' => [
+			'title_full' => 'Гарантия',
+			'title_short' => 'Гарантия',
+			'on' => true,
+			'nav_menu' => true,
+			'label' => true
+		],
+		'4' => [
+			'title_full' => 'Работаем с брендами',
+			'title_short' => 'Бренды',
+			'on' => true,
+			'nav_menu' => true,
+			'label' => true
+		],
+		'6' => [
+			'title_full' => 'Стоимость работ',
+			'title_short' => 'Расценки',
+			'on' => false,
+			'nav_menu' => true,
+			'label' => true
+		],
+		'5' => [
+			'title_full' => 'Для связи',
+			'title_short' => 'Для связи',
+			'on' => true,
+			'nav_menu' => true,
+			'label' => true
+		],
 	],
 	'slides' => [
 		'/images/slides/img_2.png',
@@ -24,4 +68,5 @@ return [
 		'/images/slides/img_1.png',
 	],
 	'emailRecall' => 'Jim7kzn@gmail.com',
+	'telegramAPI' => '6203818836:AAG2A9QEeCO67MdoCXdehvtZbKhwCZ_T5Ro',
 ];
--- frontend/views/site/block5.php
+++ frontend/views/site/block5.php
@@ -6,11 +6,11 @@
 <div class="col-lg-5 contact">
 	<form id="contact-view">
 		<div class="form-row">
-			<div class="label">Email : </div><a href="mailto:<?= $web_mail; ?>"><?= $web_mail; ?></a>
+			<div class="label">☏</div><a href="tel:<?= $web_phone; ?>"><?= $web_phone; ?></a>
 		</div>
 
 		<div class="form-row">
-			<div class="label">Телефон : </div><a href="tel:<?= $web_phone; ?>"><?= $web_phone; ?></a>
+			<div class="label">✉</div><a href="mailto:<?= $web_mail; ?>"><?= $web_mail; ?></a>
 		</div>
 	</form>
 </div>
