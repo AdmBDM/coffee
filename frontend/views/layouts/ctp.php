48e3c68fc8bf0f027f5631ab9e1ea742a9995473
Dmitriy Belik <Jim7rfp@gmail.com>
2023-11-07 09:38:30 +0300

    2023-11-07. Добавлена Яндекс-метрика.

--- frontend/views/layouts/ctp.php
+++ frontend/views/layouts/ctp.php
@@ -29,6 +29,23 @@ use yii\web\View;
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 	<script src="https://use.fontawesome.com/fadba6afe8.js"></script>
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+		(function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+			m[i].l=1*new Date();
+			for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+			k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+		(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+		ym(95482161, "init", {
+			clickmap:true,
+			trackLinks:true,
+			accurateTrackBounce:true,
+			webvisor:true
+		});
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/95482161" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
 </head>
 <body class="d-flex flex-column h-100">
 <?php $this->beginBody() ?>
